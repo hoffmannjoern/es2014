94e92b1e43b278046b2e34a4562ec87009d0d8c9
Klemens Schölhorn <klemens@schoelhorn.eu>
2014-12-09 17:12:48 +0100

    Implement: Aufgabe 1.1

---
+++ Praktikum1/Aufgabe1/Aufgabe1.ino
@@ -0,0 +1,13 @@
+/**
+ * Aufgabe 1.1
+ */
+
+void setup() {
+  // LED 1 on danger board is connected to digital pin 6
+  pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+}
